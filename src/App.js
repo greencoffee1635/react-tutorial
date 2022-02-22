1877419be291fc913803ab5bad7cf3f486212596
greencoffee1635 <166354@naver.com>
2022-02-22 17:51:00 +0900

    Add login-out and change color tutorial code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import "./App.css";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import ChangeColor from "./components/ChangeColor";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Profile />
+      <Login />
+      <ChangeColor />
     </div>
   );
 }
