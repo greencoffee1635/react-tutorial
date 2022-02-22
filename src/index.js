25abcd386ca9275680270789a3a477eda0b76baf
greencoffee1635 <166354@naver.com>
2022-02-22 18:55:24 +0900

    Add todos code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
-import userReducer from "./features/user";
-import themeReducer from "./features/theme";
+import React from 'react'
+import { render } from 'react-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import App from './components/App'
+import rootReducer from './reducers'
 
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-    theme: themeReducer,
-  },
-});
+  reducer: rootReducer
+})
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
