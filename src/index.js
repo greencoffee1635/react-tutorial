54aeaf10eda5a5d8cb1279bc09bc8e87154a024a
greencoffee1635 <166354@naver.com>
2022-02-23 13:13:35 +0900

    Add redux toolkit crd

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import rootReducer from './reducers'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const store = configureStore({
-  reducer: rootReducer
-})
+import store from "./store";
+import { Provider } from "react-redux";
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
