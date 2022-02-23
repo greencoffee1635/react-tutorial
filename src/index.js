2d374d6d5e240a19f6afefe99f404035724dd2df
greencoffee1635 <166354@naver.com>
2022-02-23 13:03:23 +0900

    Add redux toolkit user flow

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import rootReducer from './reducers'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
+import toast, { Toaster } from 'react-hot-toast';
 
-const store = configureStore({
-  reducer: rootReducer
-})
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+      <Toaster />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
-)
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
